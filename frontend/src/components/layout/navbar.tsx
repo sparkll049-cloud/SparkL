11ee922c608d4ce221dba7dcf4ae86e0906cec8b
Marvelous Onyeka <141563399+Marwebofficial@users.noreply.github.com>
2026-09-26 10:29:33 +0100

    update frontend/src/components/layout/navbar.tsx

--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 
   const navLinks = [
     { name: "Features", href: "#features" },
-    { name: "Pricing", href: "/pricing" },
+    { name: "Pricing", href: "/dashboard/subscribe" },
     { name: "Community", href: "/community" },
     { name: "Resources", href: "#resources" },
     { name: "About", href: "#about" },
